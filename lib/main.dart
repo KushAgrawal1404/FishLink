04ad54f043a4d490c2ee2d7c9dcb6313318acffe
harsha-0110 <harshavardhan.a2020@vitstudent.ac.in>
2024-02-02 22:21:45 +0530

    Added persistent login

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:fish_link/screens/login_page.dart';
 import 'package:fish_link/screens/signup_landing_page.dart';
 import 'package:fish_link/screens/buyer_signup_page.dart';
@@ -7,10 +8,12 @@ import 'package:fish_link/homescreen/buyer_home.dart';
 import 'package:fish_link/homescreen/seller_home.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,9 +21,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/',
+      home: const AuthChecker(),
       routes: {
-        '/': (context) => const LoginPage(),
+        '/login': (context) => const LoginPage(),
         '/signup_landing': (context) => const SignupLandingPage(),
         '/buyer_signup': (context) => const BuyerSignupPage(),
         '/seller_signup': (context) => const SellerSignupPage(),
@@ -28,5 +31,56 @@ class MyApp extends StatelessWidget {
         '/seller_home': (context) => const SellerHomePage(),
       },
     );
+  }
+}
+
+class AuthChecker extends StatefulWidget {
+  const AuthChecker({Key? key}) : super(key: key);
+
+  @override
+  _AuthCheckerState createState() => _AuthCheckerState();
+}
+
+class _AuthCheckerState extends State<AuthChecker> {
+  bool _isLoading = true;
+  String? _userType;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkAuth();
+  }
+
+  Future<void> _checkAuth() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? userType = prefs.getString('userType');
+    if (userType != null) {
+      setState(() {
+        _userType = userType;
+        _isLoading = false;
+      });
+    } else {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_isLoading) {
+      return CircularProgressIndicator(); // Show loading indicator
+    } else {
+      if (_userType != null) {
+        // User is logged in, navigate to respective page based on userType
+        if (_userType == 'buyer') {
+          return const BuyerHomePage();
+        } else if (_userType == 'seller') {
+          return const SellerHomePage();
+        }
+      }
+      // User is not logged in, redirect to login page
+      return const LoginPage();
+    }
   }
 }
--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -3,10 +3,11 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:fish_link/utils/api.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   _LoginPageState createState() => _LoginPageState();
@@ -15,6 +16,22 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  late SharedPreferences _prefs;
+
+  @override
+  void initState() {
+    super.initState();
+    _initPrefs();
+  }
+
+  Future<void> _initPrefs() async {
+    _prefs = await SharedPreferences.getInstance();
+    // Check if user already logged in
+    String? userType = _prefs.getString('userType');
+    if (userType != null) {
+      _redirectToHome(userType);
+    }
+  }
 
   Future<void> _login() async {
     String email = _emailController.text.trim();
@@ -34,16 +51,11 @@ class _LoginPageState extends State<LoginPage> {
       print(responseBody);
 
       if (response.statusCode == 200) {
-        // Check the userType
-        String userType = responseBody['userType'];
-
-        // Navigate to different pages based on userType
-        if (userType == 'buyer') {
-          Navigator.pushReplacementNamed(context, '/buyer_home');
-        } else if (userType == 'seller') {
-          Navigator.pushReplacementNamed(context, '/seller_home');
-        }
-        // Add more conditions for other user types if needed
+        // Save login information
+        _prefs.setString('email', email);
+        _prefs.setString('userType', responseBody['userType']);
+        // Redirect to home
+        _redirectToHome(responseBody['userType']);
       } else {
         // Login failed, show error message
         String msg = responseBody['msg'];
@@ -59,6 +71,14 @@ class _LoginPageState extends State<LoginPage> {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('An error occurred')),
       );
+    }
+  }
+
+  void _redirectToHome(String userType) {
+    if (userType == 'buyer') {
+      Navigator.pushReplacementNamed(context, '/buyer_home');
+    } else if (userType == 'seller') {
+      Navigator.pushReplacementNamed(context, '/seller_home');
     }
   }
 
