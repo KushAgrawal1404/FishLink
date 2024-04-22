aaf27315f26ac13cf663868ba450af75f3cd8cbe
harsha-0110 <harshavardhan.a2020@vitstudent.ac.in>
2024-04-22 17:47:25 +0530

    update

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -1,5 +1,5 @@
 class Api {
-  static const String baseUrl = 'http://192.168.140.82:5000';
+  static const String baseUrl = 'http://172.16.254.71:5000';
   static const String loginUrl = '$baseUrl/api/login';
   static const String signupPath = '$baseUrl/api/signup';
   static const String forgotPasswordUrl = '$baseUrl/api/forgot-password';
@@ -12,8 +12,7 @@ class Api {
   static const String catchDetailsUrl = '$baseUrl/api/catch';
   static const String fetchbids = '$baseUrl/api/my-bids';
   static const String createRatingUrl = '$baseUrl/api/ratings';
-  static String getRatingsByCatchIdUrl(String catchId) =>
-      '$baseUrl/api/ratings/$catchId';
+  static const String getRatingsByCatchIdUrl = '$baseUrl/api/ratings';
   static const String analyticsUrl = '$baseUrl/api/analytics';
   static const String userProfileUrl = '$baseUrl/api/userProfile';
 }
