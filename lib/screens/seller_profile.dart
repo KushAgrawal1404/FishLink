97a1abfced70355da84ef9e6f93371039ca0f109
akashbijukumar <akash.bijukumar123@gmail.com>
2024-04-23 22:46:49 +0530

    Seller Profile

--- lib/screens/seller_profile.dart
+++ lib/screens/seller_profile.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'package:fish_link/utils/api.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:fish_link/utils/api.dart';
 
 class SellerProfilePage extends StatefulWidget {
   const SellerProfilePage({Key? key}) : super(key: key);
@@ -13,6 +16,10 @@ class SellerProfilePage extends StatefulWidget {
 
 class _SellerProfilePageState extends State<SellerProfilePage> {
   Map<String, dynamic>? userProfile;
+  TextEditingController _bioController = TextEditingController();
+  TextEditingController _harbourController = TextEditingController();
+  String? _selectedImagePath;
+  bool _isChanged = false;
 
   @override
   void initState() {
@@ -21,7 +28,6 @@ class _SellerProfilePageState extends State<SellerProfilePage> {
   }
 
   Future<void> fetchUserProfile() async {
-    // Fetch user profile data from your API or database
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String userId = prefs.getString('userId') ?? '';
     try {
@@ -30,13 +36,14 @@ class _SellerProfilePageState extends State<SellerProfilePage> {
       if (response.statusCode == 200) {
         setState(() {
           userProfile = json.decode(response.body);
+          _bioController.text = userProfile!['bio'] ?? '';
+          _harbourController.text = userProfile!['harbour'] ?? '';
         });
       } else {
         throw Exception('Failed to load user profile');
       }
     } catch (error) {
       print('Error fetching user profile: $error');
-      // Handle error state or display a message to the user
     }
   }
 
@@ -51,78 +58,188 @@ class _SellerProfilePageState extends State<SellerProfilePage> {
           : Container(
               color: Colors.grey[200],
               padding: EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              child: ListView(
                 children: <Widget>[
-                  Card(
-                    elevation: 3,
-                    child: Padding(
-                      padding: EdgeInsets.all(16.0),
-                      child: Row(
-                        children: [
-                          Text(
-                            'Name: ',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                            ),
-                          ),
-                          Text(
-                            '${userProfile!['name']}',
-                            style: TextStyle(fontSize: 16),
-                          ),
-                        ],
+                  GestureDetector(
+                    onTap: _pickImage,
+                    child: Container(
+                      width: 250,
+                      height: 200,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: _selectedImagePath != null
+                              ? FileImage(File(_selectedImagePath!))
+                              : userProfile!['profilePic'] != null &&
+                                      userProfile!['profilePic'] != ''
+                                  ? NetworkImage(userProfile![
+                                      'profilePic']) // Use the correct URL here
+                                  : AssetImage('assets/default_profile_pic.png')
+                                      as ImageProvider,
+                        ),
                       ),
                     ),
                   ),
-                  SizedBox(height: 10),
-                  Card(
-                    elevation: 3,
-                    child: Padding(
-                      padding: EdgeInsets.all(16.0),
-                      child: Row(
-                        children: [
-                          Text(
-                            'Email: ',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                            ),
-                          ),
-                          Text(
-                            '${userProfile!['email']}',
-                            style: TextStyle(fontSize: 16),
-                          ),
-                        ],
-                      ),
-                    ),
+                  SizedBox(height: 16),
+                  _buildProfileItemBox('Name', userProfile!['name']),
+                  _buildProfileItemBox('Email', userProfile!['email']),
+                  _buildProfileItemBox('Phone', userProfile!['phone']),
+                  _buildEditableProfileItemBox('Bio', _bioController),
+                  _buildEditableProfileItemBox('Harbour', _harbourController),
+                  ElevatedButton(
+                    onPressed: _isChanged
+                        ? updateUserProfile
+                        : null, // Disable button if nothing is changed
+                    child: Text('Save'),
                   ),
-                  SizedBox(height: 10),
-                  Card(
-                    elevation: 3,
-                    child: Padding(
-                      padding: EdgeInsets.all(16.0),
-                      child: Row(
-                        children: [
-                          Text(
-                            'Phone: ',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                            ),
-                          ),
-                          Text(
-                            '${userProfile!['phone']}',
-                            style: TextStyle(fontSize: 16),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  // Add other profile details as needed
                 ],
               ),
             ),
     );
+  }
+
+  Widget _buildProfileItemBox(String label, String value) {
+    return Card(
+      elevation: 3,
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Row(
+          children: [
+            Text(
+              '$label: ',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+            Text(
+              '$value',
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildEditableProfileItemBox(
+      String label, TextEditingController controller) {
+    return Card(
+      elevation: 3,
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '$label: ',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (_) => AlertDialog(
+                        title: Text('Edit $label'),
+                        content: TextField(
+                          controller: controller,
+                          decoration: InputDecoration(
+                            labelText: label,
+                            border: OutlineInputBorder(),
+                          ),
+                          onChanged: (_) {
+                            setState(() {
+                              _isChanged = true;
+                            });
+                          },
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () => Navigator.pop(context),
+                            child: Text('Cancel'),
+                          ),
+                          ElevatedButton(
+                            onPressed: _isChanged
+                                ? () {
+                                    updateUserProfile();
+                                    Navigator.pop(context);
+                                  }
+                                : null,
+                            child: Text('Save'),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+            SizedBox(height: 4),
+            Text(
+              controller.text,
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> updateUserProfile() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String userId = prefs.getString('userId') ?? '';
+    try {
+      // Prepare the request body
+      Map<String, dynamic> requestBody = {
+        'bio': _bioController.text,
+        'harbour': _harbourController.text,
+      };
+
+      // If a new image is selected, include it in the request
+      if (_selectedImagePath != null) {
+        List<int> imageBytes = File(_selectedImagePath!).readAsBytesSync();
+        String base64Image = base64Encode(imageBytes);
+        requestBody['profilePic'] = base64Image;
+      }
+
+      final response = await http.post(
+        Uri.parse('${Api.userProfileUrl}/update/$userId'),
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode(requestBody),
+      );
+      if (response.statusCode == 200) {
+        setState(() {
+          userProfile!['bio'] = _bioController.text;
+          userProfile!['harbour'] = _harbourController.text;
+          if (_selectedImagePath != null) {
+            userProfile!['profilePic'] = _selectedImagePath;
+          }
+          _isChanged = false; // Reset _isChanged when profile is updated
+        });
+      } else {
+        throw Exception('Failed to update user profile');
+      }
+    } catch (error) {
+      print('Error updating user profile: $error');
+    }
+  }
+
+  Future<void> _pickImage() async {
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _selectedImagePath = pickedFile.path;
+        _isChanged = true; // Set _isChanged to true when image is changed
+      });
+    }
   }
 }
