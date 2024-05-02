8bacbaef828595c7874c2fa65b3c48d302f572ad
kushagra <ramjindal1404@gmail.com>
2024-05-02 18:10:10 +0530

    UI

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -137,6 +137,16 @@ class _ChatPageState extends State<ChatPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Chat'),
+        backgroundColor: Colors.transparent,
+            flexibleSpace: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.blue, Colors.green],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+              ),
+            ),
       ),
       body: Column(
         children: [
